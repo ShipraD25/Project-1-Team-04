1d5d34a9bb52ddcc1121076b88b02116930876ec
chenricky <chenricky@gmail.com>
2019-11-10 15:50:41 -0800

    add state change message

--- auth.js
+++ auth.js
@@ -9,15 +9,17 @@
 
   //add login event. 
   //read this: https://codeburst.io/javascript-arrow-functions-for-beginners-926947fc0cdc
-  document.getElementById("btnLogin").addEventListener("click", e => {
-  //btnLogin.addEventListener("click", e => {
+  //document.getElementById("btnLogin").addEventListener("click", e => {
+  btnLogin.addEventListener("click", e => {
     //get email and password
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
     //sign in
     const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
+    //promise.catch(e => console.log(e.message));
+    //promise.catch(e => console.log("promise catch, login successfully"));
+    //promise.catch(e => window.location = 'home.html');
   });
 
   //add signup event
@@ -29,23 +31,32 @@
     //sign in
     const promise = auth.createUserWithEmailAndPassword(email, pass);
     //promise.catch(e => console.log(e.message));
+    //$("#loginState").html("<br>you are now signed up!");
+
 });
 
 //add a logout fucntion
 btnLogout.addEventListener("click", e => {
     firebase.auth().signOut();
+    console.log("logout clicked");
+    //window.location = 'auth.html';
 });
 
 
 //add a realtiem lister for auth state change
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
+        console.log("state change, login is successful");
         console.log(firebaseUser);
         //make logout btn visiable after user login
         btnLogout.classList.remove("hide");
+        $("#loginState").html("<br>you have now login!");
+        
     } else {
-        console.log("Not log in");
+        console.log("state change, Not log in");
         //make logout btn invisiable if user not login
         btnLogout.classList.add("hide");
+        $("#loginState").html("<br>you are not login!");
+
     }
 });
