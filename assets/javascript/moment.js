99934dc819b464e9b497d821f1143702f34cda7f
sreeveena <sreeveena21@gmail.com>
2019-11-13 19:41:34 -0800

    added id search in index.html.

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -83,3 +83,41 @@ function query(){
         }
     });
 }
+//weather
+const loc = document.getElementById("location");
+const temNum = document.getElementById("temperature-num");
+const temScale = document.getElementById("temperature-scale");
+const weatherCon = document.getElementById("weather-condition");
+const weatherIcon = document.getElementById("weather-icon");
+// get location
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+    });
+  } else {
+    loc.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+// get weather data according to the location
+function getWeather(lat, long) {
+  const root = "https://fcc-weather-api.glitch.me/api/current?";
+  fetch(`${root}lat=${lat}&lon=${long}`, { method: "get" })
+    .then(resp => resp.json())
+    .then(data => {
+      updateDataToUI(data.name, data.weather, data.main.temp);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+}
+// update the data from API to DOM
+function updateDataToUI(location, weather, temp) {
+  weatherIcon.innerHTML = `<img src="${weather[0].icon}" />`;
+  weatherCon.innerHTML = weather[0].main;
+  loc.innerHTML = location;
+  temNum.innerHTML = `${temp}`;
+}
+window.onload = function() {
+  getLocation();
+};
